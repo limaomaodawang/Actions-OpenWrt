f669b845ea10081d2ea400aab846a42409a68986
李毛毛 <112305664+limaomaodawang@users.noreply.github.com>
2026-05-20 23:38:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,20 +1,52 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+set -e
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+echo "=============================="
+echo " Redmi AC2100 campus build"
+echo " DIY part1: add custom packages"
+echo "=============================="
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git ua2f https://github.com/Zxilly/UA2F.git' >> feeds.conf.default
-echo 'src-git mentohust https://github.com/BoringCat/luci-app-mentohust.git' >> feeds.conf.default
+mkdir -p package/custom
+
+# 清理旧目录，避免重复包导致编译冲突
+rm -rf package/custom/ua2f
+rm -rf package/custom/ua2f-src
+rm -rf package/custom/luci-app-ua2f
+rm -rf package/custom/mentohust
+rm -rf package/custom/luci-app-mentohust
+
+rm -rf /tmp/UA2F
+rm -rf /tmp/luci-app-ua2f
+rm -rf /tmp/luci-app-mentohust
+
+# =========================================================
+# 1. UA2F 主程序
+# =========================================================
+# Zxilly/UA2F 的 OpenWrt 包在仓库 openwrt/ 子目录里，
+# 这里复制成标准 package/custom/ua2f 结构，便于 OpenWrt 识别。
+git clone --depth=1 https://github.com/Zxilly/UA2F.git /tmp/UA2F
+
+mkdir -p package/custom/ua2f
+mkdir -p package/custom/ua2f-src
+
+cp -r /tmp/UA2F/openwrt/* package/custom/ua2f/
+cp -r /tmp/UA2F/. package/custom/ua2f-src/
+
+# 修正 UA2F Makefile 的源码目录指向
+sed -i 's#PKG_BUILD_DIR:=$(CURDIR)/..#PKG_BUILD_DIR:=$(CURDIR)/../ua2f-src#g' package/custom/ua2f/Makefile
+
+# =========================================================
+# 2. UA2F LuCI 配置界面
+# =========================================================
+git clone --depth=1 https://github.com/lucikap/luci-app-ua2f.git /tmp/luci-app-ua2f
+cp -r /tmp/luci-app-ua2f/luci-app-ua2f package/custom/luci-app-ua2f
+
+# =========================================================
+# 3. MentoHUST 主程序 + LuCI 界面
+# =========================================================
+git clone --depth=1 https://github.com/sbwml/luci-app-mentohust.git /tmp/luci-app-mentohust
+
+cp -r /tmp/luci-app-mentohust/mentohust package/custom/mentohust
+cp -r /tmp/luci-app-mentohust/luci-app-mentohust package/custom/luci-app-mentohust
+
+echo "DIY part1 done."
